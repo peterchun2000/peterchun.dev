47e0eedf490f44cb7c86386455ef8b97a8913d50
Peter Chun <peterchun2000@gmail.com>
2019-06-20 16:22:56 -0400

    added netifly cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -126,6 +126,13 @@ module.exports = {
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-netlify',
+    {
+      resolve: 'gatsby-plugin-netlify-cms',
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      }
+    },
     {
       resolve: 'gatsby-plugin-google-gtag',
       options: {
